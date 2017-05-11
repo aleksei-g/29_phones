ef19f1907cd1f41cdae60b12052650c058e2f04c
aleksei-g <aleksei_g@outlook.com>
2017-05-11 14:53:49 +0300

    added standby mode

--- normalized_phone.py
+++ normalized_phone.py
@@ -3,9 +3,11 @@ from sqlalchemy.exc import SQLAlchemyError
 from config import DEST_DATABASE_URI
 from db_functions import make_session, quick_mapper
 import re
+from time import sleep
 
 
 PHONE_LENGTH = 10
+DELAY = 30
 
 
 def get_normalized_phone(phone):
@@ -16,17 +18,22 @@ def run_normalize(session, engine):
     meta = MetaData(bind=engine)
     table = Table('orders', meta, autoload=True)
     table_class = quick_mapper(table)
-    for record in session.query(table_class).all():
+    for record in session.query(table_class).filter(
+            table_class.normalized_phone.is_(None)):
         record.normalized_phone = get_normalized_phone(record.contact_phone)
 
 
 if __name__ == '__main__':
     session, engine = make_session(DEST_DATABASE_URI)
-    try:
-        run_normalize(session, engine)
-        session.commit()
-        print('Normalization is completed successfully!')
-    except SQLAlchemyError:
-        session.rollback()
-        print('DB connection error!')
-        raise
+    while True:
+        try:
+            run_normalize(session, engine)
+            session.commit()
+        except SQLAlchemyError:
+            session.rollback()
+            print('DB connection error!')
+        try:
+            sleep(DELAY)
+        except KeyboardInterrupt:
+            print('exit')
+            break
